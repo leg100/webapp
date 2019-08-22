403256f2876e057dfd6480951af759deb6f1cbae
Louis Garman <75728+leg100@users.noreply.github.com>
2019-08-22 18:00:26 +0100

    make tests pass

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "Hello World!\n")
+    self.assertEqual(hello(), "Hello Dog!\n")
 
 if __name__ == '__main__':
   unittest.main()
